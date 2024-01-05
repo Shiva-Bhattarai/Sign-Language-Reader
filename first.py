4f661d34a726f405ed76fbe7785a2c34d1ad623b
Shivam Kumar <skshivam466@gmail.com>
2024-01-05 12:38:32 +0545

    webcam reading

--- first.py
+++ first.py
@@ -1,3 +1,13 @@
 import cv2 as cv
-image = cv.imread('face.jpg')
-print(image.shape)
+
+webcam = cv.VideoCapture(0) #to cature the video from webcam
+
+while True:
+    ret, frame = webcam.read() #ret must be used in order to open the webcam frame window
+
+    cv.imshow('frame', frame)
+    if cv.waitKey(40) == ord('q'): #ord is used so that q is pressed to quit and wait key is 40 for 40 millisecond per frame
+        break
+
+webcam.release()
+cv.destroyAllWindows() #to destroy all windows
